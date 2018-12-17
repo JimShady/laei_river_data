087d2455021d5fa9a7dd62c0e2966a0bc87a20f9
James <james.david.smith@gmail.com>
2018-12-17 10:42:46 +0000

    Update main_script.R

--- main_script.R
+++ main_script.R
@@ -19,21 +19,9 @@ latlong = "+init=epsg:4326"
 ukgrid = "+init=epsg:27700"
 google = "+init=epsg:3857"
 
-## Get GPS data for one day of boats
-load('gps/Gravesend_ANSData_01_Apr_2016.Rdata')
-gps_data                  <- data
-rm(data)
-gps_data                  <- st_as_sf(gps_data, coords = c('lon', 'lat'), crs = 4326)
-gps_data                  <- st_transform(gps_data, 27700)
-gps_data                  <- gps_data[!is.na(gps_data$VESSEL_TYPE),]
-
-## Import the ship classifications and link to the gps_data
+## Import the ship classifications 
 vessel_class              <- read_csv('docs/vessel_classifications.csv')
 vessel_class$code         <- as.character(vessel_class$code)
-gps_data                  <- left_join(gps_data, vessel_class, by = c('VESSEL_TYPE' = 'code'))
-
-## Remove data from GPS data that we don't need
-gps_data                  <- gps_data[,c('group')]
 
 # Get emissions by exact cut, substance and vessel type
 emissions                 <- read_csv('emissions/inventory_export_2016.csv', col_types = cols())
@@ -52,7 +40,6 @@ emissions[emissions$ship_type == 'Passenger', 'ship_type']           <-'Passenge
 
 # Add vessel group type to the emissions, for matching with GPS data
 emissions                 <- left_join(emissions, unique(vessel_class[,c('aggregated_class', 'group')]), by = c('ship_type' = 'aggregated_class'))
-rm(vessel_class)
 
 # Now get the grid by exact cut
 grid                      <- st_read('grids/LAEIGridExtensionV2.gpkg', quiet = T)
@@ -73,31 +60,71 @@ rm(emissions, grid)
 grid_emissions$geom_group <- sapply(st_equals(grid_emissions), max)
 
 grid_emissions            <- grid_emissions %>%
-                                group_by(geom_group, pollutant, group) %>%
-                                summarise(sailing = sum(sailing),
-                                          berth   = sum(berth))
+  group_by(geom_group, pollutant, group) %>%
+  summarise(sailing = sum(sailing),
+            berth   = sum(berth))
 
 grid_emissions$geom_group <- NULL
-
-##### SO NOW PAUSING AT THIS POINT WE HAVE THE FOLLOWING
-## gps_data       : large number of GPS point, each with a group identifying each type of ship
-## grid_emissions : 192 grid exact cuts. When multiplied by pollutants (3), and emission type i.e. 'group', we end up with 1590 grid 'exact cut' polygons.
-
-## Now want to thin things out to see how I get on.
-gps_data        <- filter(gps_data, group == 2)                              # Just look at group 2
-grid_emissions  <- ungroup(grid_emissions) %>% filter(pollutant == 'NOx' & group == 2) %>% st_set_geometry('geom')   # Just look at NOx emissions for group 2
-
-## Count how many GPS points within each large emission grid
 grid_emissions$id  <- 1:nrow(grid_emissions) 
 
-gps_per_grid_id         <- st_join(gps_data, grid_emissions[,c('id')])
-gps_per_grid_id         <- data.frame(table(gps_per_grid_id$id))
-names(gps_per_grid_id)  <- c('grid_id', 'total_gps_count')
-gps_per_grid_id$grid_id <- as.integer(gps_per_grid_id$grid_id)
-grid_emissions         <- left_join(grid_emissions, gps_per_grid_id, by = c("id" = "grid_id"))
+
+## Get GPS data 
+## list GPS data
+list_of_gps_data <- list.files('gps/', full.names=T, pattern = 'Rdata')
+
+#for (i in 1:length(list_of_gps_data)) {
+for (i in 1:5) {
+  
+  print(list_of_gps_data[i])
+  
+  load(list_of_gps_data[i])
+  
+  gps_data                  <- data
+  rm(data)
+  
+  gps_data                  <- st_as_sf(gps_data, coords = c('lon', 'lat'), crs = 4326)
+  gps_data                  <- st_transform(gps_data, 27700)
+  gps_data                  <- gps_data[!is.na(gps_data$VESSEL_TYPE),]
+  
+  gps_data                  <- left_join(gps_data, vessel_class, by = c('VESSEL_TYPE' = 'code'))
+  
+  ## Remove data from GPS data that we don't need
+  gps_data                  <- gps_data[,c('group')]
+  
+  # Count, over the year in total, how many GPS points there are in each large grid square
+  gps_per_grid_id                       <- st_join(gps_data, grid_emissions[,c('id')])
+  gps_per_grid_id                       <- data.frame(table(gps_per_grid_id$id))
+  names(gps_per_grid_id)                <- c('grid_id', 'total_daily_gps_count')
+  gps_per_grid_id$grid_id               <- as.integer(gps_per_grid_id$grid_id)
+  grid_emissions                        <- left_join(grid_emissions, gps_per_grid_id, by = c("id" = "grid_id"))
+  
+  if (i == 1) {
+    grid_emissions$total_annual_gps_count <- grid_emissions$total_daily_gps_count
+    grid_emissions$total_daily_gps_count  <- NULL
+    rm(gps_per_grid_id)
+  } else {
+    grid_emissions$total_annual_gps_count <- grid_emissions$total_annual_gps_count + grid_emissions$total_daily_gps_count
+    grid_emissions$total_daily_gps_count  <- NULL
+    rm(gps_per_grid_id)   
+  }
+  
+  rm(gps_data)
+  
+}
+                
+## At this point code deals with counting all the GPS points inside the large squares but not small ones
+
+
+
+
+
+
+
+
+
 
 ## So now make a grid of 50m polygons which are inside the larger polygons
-fifty_m_grid    <- st_make_grid(grid_emissions, cellsize = 50, what = 'polygons') %>% st_sf()
+fifty_m_grid    <- st_make_grid(grid_emissions, cellsize = 100, what = 'polygons') %>% st_sf()
 
 # At this point need to count the GPS points instead my smaller polygons
 fifty_m_grid$id       <- 1:nrow(fifty_m_grid)
